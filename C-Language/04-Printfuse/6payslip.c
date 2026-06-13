667e008d0c2fc8dc2825c752d1bbae665309f092
DarkTrace <adityajhasusari@gmail.com>
2026-06-13 22:07:17 +0530

    Updated

---
+++ C-Language/04-Printfuse/6payslip.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("EMPLOYEE PAYSLIP\n");
+        printf("Name : Vikash\n");
+    printf("Salary : 90000");
+    printf("Date '13-06-26'\n");
+    return 0;
+}
---
+++ C-Language/04-Printfuse/7invoice.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Item\tQty\n");
+    printf("Pen\t2\n");
+    printf("Book\t1\n");
+    printf("Bag\t1");
+    return 0;
+}
